8f0e4503c80d7d46040f4b9cb1ad5db625a3ea88
Naveen <spnaveenshanmugam@outlook.com>
2022-05-23 15:08:30 +0530

    resolving the conflict

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1_Github
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("");
             Math math = new Math();
             Console.WriteLine("Add result is"+math.Add(2,3));
             Console.ReadLine();
